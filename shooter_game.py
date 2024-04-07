4327aede818200335c9f6447edbc4d1594c16e2d
frederika <164749454+frederika@users.noreply.github.com>
2024-04-07 15:16:24 +0300

    Update shooter_game.py
    Додали здатність ракети стріляти

--- shooter_game.py
+++ shooter_game.py
@@ -1,5 +1,6 @@
 #Створи власний Шутер!
 
+from typing import Any
 from pygame import *
 from random import randint
 # фонова музика
@@ -9,17 +10,20 @@ mixer.music.play()
 fire_sound = mixer.Sound('fire.ogg')
 
 font.init()
-font1 = font.Font(None,36)
-
-score = 0
-lost = 0
-
+font1 = font.Font(None, 36)
+font2 = font.Font(None, 80)
+win = font2.render("You win", True, (255, 255, 255))
+lost = font2.render("You lose", True, (180, 0, 0))
 
 # нам потрібні такі картинки:
 img_back = "galaxy.jpg"  # фон гри
 img_hero = "rocket.png"  # герой
 img_enemy = "ufo.png"
+img_bullet = "bullet.png"
 
+score = 0
+lost = 0
+max_lose = 3
 
 # клас-батько для інших спрайтів
 class GameSprite(sprite.Sprite):
@@ -59,14 +63,23 @@ class Player(GameSprite):
 
     # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
     def fire(self):
-        pass
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
 
 class Enemy(GameSprite):
     def update(self):
         self.rect.y += self.speed
+        global lost
         if self.rect.y > win_height:
             self.rect.x = randint(80, win_width-80)
             self.rect.y = 0
+            lost = lost + 1
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
 
 
 
@@ -81,9 +94,12 @@ background = transform.scale(image.load(img_back), (win_width, win_height))
 # створюємо спрайти
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 monsters = sprite.Group()
-for i in range(1,6):
+for i in range(1, 6):
     monster = Enemy(img_enemy, randint(80, win_width-80), -40, 80, 50, randint(1,5))
     monsters.add(monster)
+
+bullets = sprite.Group()
+
 
 
 # змінна "гра закінчилася": як тільки вона стає True, в основному циклі перестають працювати спрайти
@@ -99,26 +115,31 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
 
 
     if not finish:
         # оновлюємо фон
         window.blit(background, (0, 0))
-        text = font1.render("Рахунок: " + str(score), 1, (255, 255, 255))
+        text = font1.render('Рахунок: '+ str(score), 1, (255, 255, 255))
         window.blit(text, (10, 20))
 
-        text_lost = font1.render("Пропущено: " + str(score), 1, (255, 255, 255))
+        text_lost = font1.render('Пропущено: '+ str(lost), 1, (255, 255, 255))
         window.blit(text_lost, (10, 50))
-
 
         # рухи спрайтів
         ship.update()
-        monster.update()
+        monsters.update()
+        bullets.update()
 
 
         # оновлюємо їх у новому місці при кожній ітерації циклу
         ship.reset()
-        monster.draw(window)
+        monsters.draw(window)
+        bullets.draw(window)
 
 
         display.update()
